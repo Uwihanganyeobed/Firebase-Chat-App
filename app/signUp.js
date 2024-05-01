a2a163be648416ee3a5ec527ad0717032caef9a0
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-05-01 22:09:26 +0200

    al

--- app/signUp.js
+++ app/signUp.js
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function SignIn() {
+export default function SignUp() {
   return (
     <View>
-      <Text>signIn</Text>
+      <Text>signUp</Text>
     </View>
   )
 }
