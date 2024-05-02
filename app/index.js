4246c9cc5196e8ea9f78585ee1e1d7d7d6516bdb
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-05-02 09:10:18 +0200

    setups

--- app/index.js
+++ app/index.js
@@ -1,15 +1,44 @@
-import { View, Text, TouchableOpacity, Button, ActivityIndicator } from 'react-native'
+import { View, Text, TouchableOpacity, Button, ActivityIndicator, ScrollView } from 'react-native'
 import React, { useEffect, useState } from 'react'
+import * as Icon from 'react-native-feather'
 
 export default function StartPage() {
-   const [state, setState]=useState(0);
+   const [post, setPost]=useState([]);
+   const [isLoading, setIsLoading]=useState(true);
+   const HandleData= async()=>{
+    try{
+      const responseData=  await fetch('https://jsonplaceholder.typicode.com/posts');
+      const data= await responseData.json();
+      setIsLoading(false);
+      setPost(data);
+    }
+    catch(err){
+      console.log('erro fetching data', err);
+      setIsLoading(false);
+    }
+  };
    useEffect(()=>{
-    
+    HandleData();
    },[])
   return (
+    <ScrollView>
     <View className='flex-1 justify-center items-center'>
-      <ActivityIndicator size='large' color='gray' />
-      <Button title='Click here to fetch data'onPress={()=>alert('Data fetched')} />
+      {isLoading?(
+        <ActivityIndicator size='large'color='blue'/>
+      ):(
+      post.map((data, index)=>(
+          <View key={index} className='flex-2 justify-center items-center w-300'>
+                  <Icon.ArrowDownLeft width={30} height={50} stroke='yellow' strokeWidth={5}/>
+            <Text  className='text-green-500'>{data.title}</Text>
+            <Text className='text-yellow-500'>{data.body}</Text>
+        
+          </View>
+        ))
+      )}
+      {/* <Button title='Click here to fetch data'onPress={HandleData} /> */}
+
     </View>
-  )
+    </ScrollView>
+
+  );
 }
