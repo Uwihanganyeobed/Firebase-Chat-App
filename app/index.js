12a602ec4e82f65e869d7b9ea806234d6e7057dd
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-05-02 08:21:41 +0200

    applying data

--- app/index.js
+++ app/index.js
@@ -1,11 +1,15 @@
 import { View, Text, TouchableOpacity, Button, ActivityIndicator } from 'react-native'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function StartPage() {
    const [state, setState]=useState(0);
+   useEffect(()=>{
+    
+   },[])
   return (
     <View className='flex-1 justify-center items-center'>
       <ActivityIndicator size='large' color='gray' />
+      <Button title='Click here to fetch data'onPress={()=>alert('Data fetched')} />
     </View>
   )
 }
