87e60539d90f3fa1a743d59b2d60dc345d583f3a
Mayor vargas <122959151+Uwihanganyeobed@users.noreply.github.com>
2024-07-17 19:39:33 +0200

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 import { View, Text, TouchableOpacity, Button, ActivityIndicator, ScrollView } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import * as Icon from 'react-native-feather'
 
 export default function StartPage() {
